55c5d1280bd5396087141c6867e5b0881e26d470
Ha Nguyen <hnn152151927@gmail.com>
2022-11-15 20:54:05 +0700

    FE: add gg analytics

--- frontend-user/pages/_app.tsx
+++ frontend-user/pages/_app.tsx
@@ -1,5 +1,6 @@
 import AOS from "aos"
 import "aos/dist/aos.css"
+import Script from "next/script"
 import { useEffect } from "react"
 import "tippy.js/dist/tippy.css"
 import "../styles/globals.css"
@@ -12,7 +13,24 @@ function MyApp({ Component, pageProps }) {
     })
   }, [])
 
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-6JQ1NZ8D6T"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-6JQ1NZ8D6T');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
